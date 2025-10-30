c0498156ee93af6e7c464cf7d3f5235a29d531d8
Sandeep-arlee <sandeeparlee@gmail.com>
2025-10-30 18:03:59 +0530

    auto-save: 2025-10-30 18:03:59

--- public/env.js
+++ public/env.js
@@ -1,7 +1,3 @@
-/* public/env.js (GitHub Pages runtime env) */
-window._env_ = {
-  REACT_APP_RAZORPAY_KEY: 'rzp_live_RYAbYbd9Etusqz'
-};
 <script>
   window._env_ = { REACT_APP_RAZORPAY_KEY: 'rzp_live_RYAbYbd9Etusqz' };
 </script>
