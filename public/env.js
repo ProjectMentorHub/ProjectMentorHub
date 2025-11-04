111d5cc33008df18005bed14e474843b58a49499
Sandeep-arlee <sandeeparlee@gmail.com>
2025-11-04 12:05:37 +0530

    auto-save: 2025-11-04 12:05:37

--- public/env.js
+++ public/env.js
@@ -1,7 +1,7 @@
 /* public/env.js */
 window._env_ = {
   REACT_APP_RAZORPAY_KEY: 'rzp_live_RYAbYbd9Etusqz',
-  REACT_APP_ADMIN_ACCESS_HASH: 'bf4ad3e4875bec4155c2e597d3444107f13270672d43cbe332e89fd44a2cdb54', // sha256('admin-demo-access') – override in production
+  REACT_APP_ADMIN_ACCESS_HASH: 'bf4ad3e4875bec4155c2e597d3444107f13270672d43cbe332e89fd44a2cdb54', // sha256('shiva-love-ruthu') – override in production
   REACT_APP_ADMIN_ACCESS_HINT: '',
   REACT_APP_ANALYTICS_EMBED_URL: ''
 };
